eed2bbd4664b017120cea8967df048c58f330003
younglu <younglu@tencent.com>
2022-05-15 19:41:42 +0800

    feat: update prod config file

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,15 +1,6 @@
 import prod from './prod'
+import local from './local'
 
-const local = {
-  db: {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '123456',
-    database: 'basf'
-  },
-  signinSessionKey: 'localtestkey'
-}
 const config = process.env.NODE_ENV === 'production' ? prod : local
 
 export default config
---
+++ backend/src/config/local.ts
@@ -0,0 +1,12 @@
+const config = {
+  db: {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '123456',
+    database: 'basf'
+  },
+  signinSessionKey: 'localtestkey'
+}
+
+export default config
